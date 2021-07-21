bc1f0f01622a20bf4d9ec1b54f1e34ccc7775571
Adam-smh <82158988+Adam-smh@users.noreply.github.com>
2021-07-21 15:36:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,5 +15,12 @@ def jokes():
            "{}".format(data['icon_url'], data)
 
 
+@app.route("/category", methods=["GET"])
+def smh():
+    url = "https://api.chucknorris.io/jokes/categories"
+    ata = requests.get(url).json()
+    return "{}".format(ata)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
